d708b7d0f3125f3497646d67755ef8625a3f3f26
Noel Peterson <nmpeterson@users.noreply.github.com>
2016-01-25 11:29:58 -0600

    Added README with directions, and created folders for inputs/outputs

--- accessibilities/runAccessibilities.bat
+++ accessibilities/runAccessibilities.bat
@@ -1,3 +1,5 @@
+::  Install 64-bit Anaconda from www.continuum.io/downloads to C:\Anaconda
 SET PYPATH=C:\Anaconda\python.exe
 %PYPATH% cmapaccess.py near_taps
 %PYPATH% cmapaccess.py accessibilities
+PAUSE
