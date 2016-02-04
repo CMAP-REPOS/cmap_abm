8be7f8811e4adfab9a8f32842d8bea61b9010df3
Noel Peterson <nmpeterson@users.noreply.github.com>
2016-02-04 09:18:30 -0600

    Tweaks to improve model reliability

--- runCTRAMP-SingleProcess.bat
+++ runCTRAMP-SingleProcess.bat
@@ -4,7 +4,7 @@ set RUNTIME=Y:/{{{TEMPLATE}}}/model
 set JAVA_PATH=C:/Program Files/Java/jdk1.7.0_13
 
 rem ##########  Run CTRAMP  ##############
-"%JAVA_PATH%\bin\java" -Xmx64000m -cp "%RUNTIME%;%RUNTIME%/exec/jxl.jar;%RUNTIME%/exec/jppf-5.0.4-lib/*;%RUNTIME%/exec/*;config" -Dlog4j.configuration=log4j.xml -server -Djppf.config=jppf-guiLocal.properties com.pb.cmap.tourBased.CmapTourBasedModel "cmap" -iteration 1 -sampleRate %1 -sampleSeed 0
+"%JAVA_PATH%\bin\java" -Xmx120000m -cp "%RUNTIME%;%RUNTIME%/exec/jxl.jar;%RUNTIME%/exec/jppf-5.0.4-lib/*;%RUNTIME%/exec/*;config" -Dlog4j.configuration=log4j.xml -server -Djppf.config=jppf-guiLocal.properties com.pb.cmap.tourBased.CmapTourBasedModel "cmap" -iteration 1 -sampleRate %1 -sampleSeed 0
 
 rem ##########  Run CTRAMP Remote Debug ##
 rem "%JAVA_PATH%\bin\java" -Xdebug -Xrunjdwp:transport=dt_socket,address=1044,server=y,suspend=y -Xmx32000m -cp "%RUNTIME%;%RUNTIME%/exec/jxl.jar;%RUNTIME%/config/jppf-2.4/build/*;%RUNTIME%/config/jppf-2.4/lib/slf4j/*;%RUNTIME%/exec/*;config" -Dlog4j.configuration=log4j.xml -server -Djppf.config=jppf-clientLocal.properties com.pb.cmap.tourBased.CmapTourBasedModel "cmap" -iteration 1 -sampleRate %1 -sampleSeed 0
