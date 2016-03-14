bc745c6a8eb3a3e66acd09fc7cf0543efb2ee7d5
Noel Peterson <nmpeterson@users.noreply.github.com>
2016-03-14 17:24:28 -0500

    Continued reorganization of code

--- results_analysis/results_analysis.py
+++ results_analysis/results_analysis.py
@@ -2,7 +2,7 @@
 '''
     results_analysis.py
     Author: npeterson
-    Revised: 3/11/16
+    Revised: 3/14/16
     ---------------------------------------------------------------------------
     A module for reading TMM output files and matrix data into an SQL database
     for querying and summarization.
@@ -10,59 +10,58 @@
 '''
 from abm import *
 
-def main(
-        base_dir=r'X:\new_server_tests\test_5pct',
-        test_dir=r'X:\new_server_tests\test_50pct_newsocec_3sp',
-        base_pct=0.05,
-        test_pct=0.5,
-        base_build=False,
-        test_build=False
-    ):
-    print '\n{0:*^50}'.format(' P R O C E S S I N G ')
-    print '\n{0:=^50}\n'.format(' BASE NETWORK ')
-    base = ABM(base_dir, base_pct, base_build)
-    base.open_db()
-    base.print_mode_share()
-    base.print_transit_stats()
-    base.print_ptrips_by_class()
-    base.print_vmt_by_speed()
-    base.close_db()
-    print base
-    print ' '
+# Set Base ABM parameters
+BASE_DIR = r'X:\new_server_tests\test_5pct'
+BASE_PCT = 0.05
+BASE_BUILD = False
 
-    print '\n{0:=^50}\n'.format(' TEST NETWORK ')
-    test = ABM(test_dir, test_pct, test_build)
-    test.open_db()
-    test.print_mode_share()
-    test.print_transit_stats()
-    test.print_ptrips_by_class()
-    test.print_vmt_by_speed()
-    test.close_db()
-    print test
-    print ' '
+# Set Test ABM parameters
+TEST_DIR = r'X:\new_server_tests\test_50pct_newsocec_3sp'
+TEST_PCT = 0.5
+TEST_BUILD = False
 
-    print '\n{0:=^50}\n'.format(' COMPARISON ')
-    comp = Comparison(base, test)
-    print comp
-    print ' '
+# Initialize Base ABM object & print summaries
+print '\n{0:*^50}'.format(' P R O C E S S I N G ')
+print '\n{0:=^50}\n'.format(' BASE NETWORK ')
+base = ABM(BASE_DIR, BASE_PCT, BASE_BUILD)
+base.open_db()
+base.print_mode_share()
+base.print_transit_stats()
+base.print_ptrips_by_class()
+base.print_vmt_by_speed()
+base.close_db()
+print base
+print ' '
 
-    print '\n{0:*^50}'.format(' R E S U L T S ')
-    comp.open_dbs()
+# Initialize Test ABM object & print summaries
+print '\n{0:=^50}\n'.format(' TEST NETWORK ')
+test = ABM(TEST_DIR, TEST_PCT, TEST_BUILD)
+test.open_db()
+test.print_mode_share()
+test.print_transit_stats()
+test.print_ptrips_by_class()
+test.print_vmt_by_speed()
+test.close_db()
+print test
+print ' '
 
-    comp.print_mode_share_change()
-    comp.print_transit_stats_change()
-    comp.print_ptrips_by_class_change()
-    comp.print_vmt_by_speed_change()
-    comp.print_new_all()
-    comp.print_new_auto()
-    comp.print_new_dtt()
-    comp.print_new_wtt()
-    comp.print_new_other()
-    #comp.print_auto_trips_affected()  ### MAJOR MEMORY HOG
+# Initialize Test vs. Base Comparison object
+print '\n{0:=^50}\n'.format(' COMPARISON ')
+comp = Comparison(base, test)
+print comp
+print ' '
 
-    comp.close_dbs()
-
-    return (base, test, comp)
-
-if __name__ == '__main__':
-    main()
+# Print comparison summaries
+print '\n{0:*^50}'.format(' R E S U L T S ')
+comp.open_dbs()
+comp.print_mode_share_change()
+comp.print_transit_stats_change()
+comp.print_ptrips_by_class_change()
+comp.print_vmt_by_speed_change()
+comp.print_new_all()
+comp.print_new_auto()
+comp.print_new_dtt()
+comp.print_new_wtt()
+comp.print_new_other()
+#comp.print_auto_trips_affected()  ### MAJOR MEMORY HOG
+comp.close_dbs()
