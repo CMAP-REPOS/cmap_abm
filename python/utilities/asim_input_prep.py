2de5e3fe57d41d8b5c858d858ae0acc9edb87ca2
tedlini <teddy.lin@rsgin.com>
2022-12-19 09:39:47 -0800

    -added script to add external zones -updated filenames and filepaths

--- python/utilities/asim_input_prep.py
+++ python/utilities/asim_input_prep.py
@@ -13,15 +13,21 @@ class ASimInputPrep():
         print("Preparing Inputs for ActivitySim...")
 
     def __call__(self, parms):
-        asim_inputs = os.environ['ASIM_INPUTS']
-        model_inputs = os.environ['INPUT_FOLDER']
+        asim_inputs = os.environ['ASIM_INPUT']
+        twozone_input = os.environ['TWOZONE_INPUT']
         mazfile_name = parms['mazfile_name']
         mazid = parms['maz_shape_maz_id']
         tazid = parms['maz_shape_taz_id']
-        mazshapeSf = gpd.read_file(os.path.join(model_inputs, mazfile_name))
+        mazshapeSf = gpd.read_file(os.path.join(twozone_input, mazfile_name))
         mazshapeSf.rename(columns = {mazid: "MAZ", tazid: "TAZ"}, inplace = True)
-        mazshapeSf[['MAZ', 'TAZ']].to_csv(os.path.join(asim_inputs, "MAZ.csv"), index = False)
-        tazList = mazshapeSf['TAZ'].unique()
-        tazList.sort()
-        pd.DataFrame(tazList, columns = ['TAZ']).to_csv(os.path.join(asim_inputs, "TAZ.csv"), index = False)
+        mazList = mazshapeSf[['MAZ', 'TAZ']]
+        maxMAZ = mazList['MAZ'].max()
+        maxTAZ = mazList['TAZ'].max()
+        for i in range(maxTAZ + 1, maxTAZ + parms['land_use']['numExtTAZ'] + 1):
+            externalList = {'MAZ': i + (maxMAZ - maxTAZ), 'TAZ': i}
+            mazList = mazList.append(externalList, ignore_index = True) # add external MAZs and TAZs
+        mazList.to_csv(os.path.join(asim_inputs, "maz.csv"), index = False)
+        #tazList = mazshapeSf['TAZ'].unique()
+        #tazList.sort()
+        #pd.DataFrame(tazList, columns = ['TAZ']).to_csv(os.path.join(asim_inputs, "taz.csv"), index = False)
         
