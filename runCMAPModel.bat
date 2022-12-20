7011445d8dcda7f44c95b46680b4066247026e68
tedlini <teddy.lin@rsgin.com>
2022-12-19 16:55:02 -0800

    reduced to 2 iterations, cleaned up script, and updated filepaths

--- runCMAPModel.bat
+++ runCMAPModel.bat
@@ -1,88 +1,69 @@
-:: This is the entire CMAP model runner script. 
-:: 
+:: This is the entire CMAP model runner script.
+::
 :: Ted Lin, RSG
 
 :: READ FIRST: cmap_abm\README.md
 
 @ECHO OFF
-
+set SCEN_NAME=%1
 :: Scenario Paths - These define the inputs and outputs
 SET BASE_PATH=C:\projects\cmap_activitysim\cmap_abm
 SET MODEL_PARAMETERS=%BASE_PATH%\model_params.yaml
-      
-SET PROJECT=%BASE_PATH%\CMAP-ABM\CMAP-ABM.emp
-SET INPUT_FOLDER=%BASE_PATH%\inputs
-SET WARM_START=%BASE_PATH%\emme_inputs
-SET EMME_OUTPUT=%BASE_PATH%\emme_outputs
-SET SCENARIO_OUTPUT=%BASE_PATH%\outputs
-SET LOG_OUT=%BASE_PATH%\Reports
-MD %SCENARIO_OUTPUT%
-MD %SCENARIO_OUTPUT%\activitysim_outputs
-MD %SCENARIO_OUTPUT%\activitysim_logs
 
-:: Python paths - EMMEPY should point to the Emme installation, ANACONDA should point to the 
-:: Anaconda environment; both should point directly to the Python executable.
-SET EMMEPY="C:\Program Files\INRO\Emme\Emme 4\Emme-4.4.5.1\Python27\python.exe"
-SET PYTHONPATH="C:\Program Files\INRO\Emme\Emme 4\Emme-4.4.5.1\Python27\lib"
-
+SET PROJECT=%BASE_PATH%\emme\CMAP-ABM\CMAP-ABM.emp
+SET TWOZONE_INPUT=%BASE_PATH%\twozone_inputs
+SET EMME_INPUT=%BASE_PATH%\emme\emme_inputs
+SET EMME_OUTPUT=%BASE_PATH%\emme\emme_outputs
 SET ASIM=%BASE_PATH%\activitysim
-SET ASIM_INPUTS=%ASIM%\data
-SET ASIM_OUTPUTS=%ASIM%\output
-::#TODO: check output folder, delete all prior outputs
-:: Removing old outputs
-::DEL /y %SCENARIO_OUTPUT%
-::DEL /y %LOG_OUT%\*
+SET ASIM_INPUT=%BASE_PATH%\model_runs\%SCEN_NAME%\inputs
+SET ASIM_OUTPUT=%BASE_PATH%\model_runs\%SCEN_NAME%\outputs
+MD %ASIM_OUTPUT%
+
+:: Python paths - EMMEPY should point to the Emme installation
+SET EMMEPY="C:\Program Files\INRO\Emme\Emme 4\Emme-4.6.1\Python37\python.exe"
+SET PYTHONPATH="C:\Program Files\INRO\Emme\Emme 4\Emme-4.6.1\Python37\Lib"
+::SET EMMEPY="C:\Program Files\INRO\Emme\Emme 4\Emme-4.4.5.1\Python27\python.exe"
+::SET PYTHONPATH="C:\Program Files\INRO\Emme\Emme 4\Emme-4.4.5.1\Python27\Lib"
 
 :: Prepare EMMEBank - import network; create and import warmstart matrices
-::%EMMEPY% %BASE_PATH%\python\import\importScenarios.py
-::%EMMEPY% %BASE_PATH%\python\import\importScalars.py
-::%EMMEPY% %BASE_PATH%\python\import\importwsmatrices.py
-::%EMMEPY% %BASE_PATH%\python\import\importConnectors.py
+%EMMEPY% %BASE_PATH%\python\import\importScenarios.py
+%EMMEPY% %BASE_PATH%\python\import\importScalars.py
+%EMMEPY% %BASE_PATH%\python\import\importwsmatrices.py
+%EMMEPY% %BASE_PATH%\python\import\importTransitConnectors.py
 
-:: to be removed
-::%EMMEPY% %BASE_PATH%\python\export_trn_los_maps.py
+:: Create an OMX stating work county for each work zone
+::%EMMEPY% %BASE_PATH%\python\County_script.py
 
 :: Iteration 0
 :: Skim highway and transit for ActivitySim
-::%EMMEPY% %BASE_PATH%\python\cmap_assignment_runner.py 0
-::%EMMEPY% %BASE_PATH%\python\cmap_transit_assignment_runner.py 0
+%EMMEPY% %BASE_PATH%\python\cmap_assignment_runner.py 0
+%EMMEPY% %BASE_PATH%\python\cmap_transit_assignment_runner.py 0
 
 :: Iteration 1
 :: Run ActivitySim
-::CALL runAsim.bat
+CALL runAsim.bat 1
+copy %ASIM_INPUT%\taz_skims.omx %ASIM_INPUT%\taz_skims_iter0.omx
 :: Run Emme Assignment
-::%EMMEPY% %BASE_PATH%\python\import\importMatrices.py 1
-::%EMMEPY% %BASE_PATH%\python\cmap_assignment_runner.py 1
-::%EMMEPY% %BASE_PATH%\python\cmap_transit_assignment_runner.py 1
+%EMMEPY% %BASE_PATH%\python\import\importMatrices.py 1
+%EMMEPY% %BASE_PATH%\python\cmap_assignment_runner.py 1
+%EMMEPY% %BASE_PATH%\python\cmap_transit_assignment_runner.py 1
 
 :: Iteration 2
 :: Run ActivitySim
-::CALL runAsim.bat
+CALL runAsim.bat 2
+copy %ASIM_INPUT%\taz_skims.omx %ASIM_INPUT%\taz_skims_iter1.omx
 :: Run Emme Assignment
-::%EMMEPY% %BASE_PATH%\python\import\importMatrices.py 2
-::%EMMEPY% %BASE_PATH%\python\cmap_assignment_runner.py 2
-::%EMMEPY% %BASE_PATH%\python\cmap_transit_assignment_runner.py 2
+%EMMEPY% %BASE_PATH%\python\import\importMatrices.py 2
+%EMMEPY% %BASE_PATH%\python\cmap_assignment_runner.py 2
+%EMMEPY% %BASE_PATH%\python\cmap_transit_assignment_runner.py 2
 
-:: Iteration 3
-:: Run ActivitySim
-::CALL runAsim.bat
-:: Run Emme Assignment
-::%EMMEPY% %BASE_PATH%\python\import\importMatrices.py 3
-::%EMMEPY% %BASE_PATH%\python\cmap_assignment_runner.py 3
-::%EMMEPY% %BASE_PATH%\python\cmap_transit_assignment_runner.py 3
-
-::
 :: OUTPUTS
-::
-
-::#TODO: Uncomment next four before flight, Remove >nul before flight next three lines
-::ECHO Copying ActivitySim files to output folder... 
-::COPY %ASIM%\output\*.omx %SCENARIO_OUTPUT%\activitysim_outputs > nul 
-::COPY %ASIM%\output\final*.csv %SCENARIO_OUTPUT%\activitysim_outputs > nul
-::COPY %ASIM%\output\*activitysim.log %SCENARIO_OUTPUT%\activitysim_logs > nul
 
 ::#TODO: copy outputs to destination folder
 
 ::#TODO: run visualizer, send to output folder
+
+:: export skims maps from Emme
+::%EMMEPY% %BASE_PATH%\python\export_trn_los_maps.py
 
 ::#TODO: cleanup
