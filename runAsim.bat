6c14b0039b7f44b6a294d624014ca49ccd19d937
tedlini <teddy.lin@rsgin.com>
2023-01-26 18:51:22 -0800

    updated python env activation

--- runAsim.bat
+++ runAsim.bat
@@ -1,10 +1,10 @@
 ::@ECHO OFF
 set ITERATION=%1
 SETLOCAL EnableDelayedExpansion
-SET ANACONDA=C:\Users\%USERNAME%\.conda\envs\cmapasim\python.exe
+::SET ANACONDA=C:\Users\%USERNAME%\.conda\envs\cmapasim\python.exe
 SET ANACONDA_DIR=C:\ProgramData\Anaconda3
 SET PATH=%ANACONDA_DIR%\Library\bin;%PATH%
-SET PYTHONPATH="C:\Users\%USERNAME%\.conda\envs\cmapasim\lib"
+::SET PYTHONPATH="C:\Users\%USERNAME%\.conda\envs\cmapasim\lib"
 
 :: setup paths to Python application, Conda script, etc.
 SET CONDA_ACT=%ANACONDA_DIR%\scripts\activate.bat
@@ -16,15 +16,14 @@ ECHO Activate ActivitySim Environment....
 CALL %CONDA_ACT% cmapasim
 
 set MKL_NUM_THREADS=1
-::ECHO %PYTHONPATH%
 
 :: Prepare files for ActivitySim
 IF %ITERATION% EQU 1 (
-    %ANACONDA% python\prepAsim.py
+    python python\prepAsim.py
 )
 
 :: Crop files for testing ActivitySim
-::%ANACONDA% python\crop.py little
+python python\crop.py little
 
 :: Run ActivitySim
-%ANACONDA% %ASIM%\simulation.py -c %ASIM%\configs -o %ASIM_OUTPUT% -d %ASIM_INPUT%
+python %ASIM%\simulation.py -c %ASIM%\configs -o %ASIM_OUTPUT% -d %ASIM_INPUT%
