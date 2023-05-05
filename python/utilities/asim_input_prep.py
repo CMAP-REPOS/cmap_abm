d2e9804b51acad35d74c95af8b202e8d22275ac2
tedlini <teddy.lin@rsgin.com>
2023-05-05 13:21:16 -0700

    updated read shapefiles with geopandas upgrade

--- python/utilities/asim_input_prep.py
+++ python/utilities/asim_input_prep.py
@@ -18,14 +18,16 @@ class ASimInputPrep():
         mazfile_name = parms['mazfile_name']
         mazid = parms['maz_shape_maz_id']
         tazid = parms['maz_shape_taz_id']
-        mazshapeSf = gpd.read_file(os.path.join(twozone_input, mazfile_name))
+        mazshapeSf = gpd.read_file(os.path.join(twozone_input, mazfile_name), engine = 'pyogrio')
         mazshapeSf.rename(columns = {mazid: "MAZ", tazid: "TAZ"}, inplace = True)
         mazList = mazshapeSf[['MAZ', 'TAZ']]
         maxMAZ = mazList['MAZ'].max()
         maxTAZ = mazList['TAZ'].max()
         for i in range(maxTAZ + 1, maxTAZ + parms['land_use']['numExtTAZ'] + 1):
-            externalList = {'MAZ': i + (maxMAZ - maxTAZ), 'TAZ': i}
-            mazList = mazList.append(externalList, ignore_index = True) # add external MAZs and TAZs
+            #externalList = {'MAZ': i + (maxMAZ - maxTAZ), 'TAZ': i}
+            #mazList = mazList.append(externalList, ignore_index = True) # add external MAZs and TAZs
+            externalList = pd.DataFrame([[i + (maxMAZ - maxTAZ), i]], columns=['MAZ', 'TAZ'])
+            mazList = pd.concat([mazList, externalList], ignore_index = True)
         mazList.to_csv(os.path.join(asim_inputs, "maz.csv"), index = False)
         #tazList = mazshapeSf['TAZ'].unique()
         #tazList.sort()
